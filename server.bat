76239b7d0c6690b8522c6b0d58d92b87537eba65
Emelianchenko Maksim <beta3-z@mail.ru>
2015-10-19 23:17:19 +0300

    refactoring
    Lol, .bat rafctoring xD

--- server.bat
+++ server.bat
@@ -3,19 +3,13 @@
 D:
 set SRV_PATH=D:\server\
 
-if "%1" == "auto" goto auto
-if "%1" == "stop" goto stop
+if "%1" == "auto" (
+	set bat_name=server_command.bat
+) else if "%1" == "stop" (
+	set bat_name=server_stop.bat
+) else (
+	start server_command.bat %1 %2 %3 %4 %5 %6 %7 %8 %9
+	exit
+)
 
-goto command
-
-:command
-start server_command.bat %1 %2 %3 %4 %5 %6 %7 %8 %9
-exit
-
-:stop
-start server_stop.bat %2 %3 %4 %5 %6 %7 %8 %9
-exit
-
-:auto
-start server_auto.bat %2 %3 %4 %5 %6 %7 %8 %9
-exit
+start /B %bat_name% %2 %3 %4 %5 %6 %7 %8 %9
--- server_command.bat
+++ server_command.bat
@@ -1,30 +1,22 @@
 @echo off
 
-if not "%1" == "" goto before_command
+if "%1" == "php" (
+	set app_path=php-5.6
+	set start_cmd=php-cgi.exe -b 24055 -c %SRV_PATH%\php-5.6\php.ini
+	
+) else if "%1" == "nginx" (
+	set app_path=nginx-1.8.0
+	set start_cmd=nginx.exe
+	
+) else if "%1" == "mysql" (
+	set app_path=mysql-5.6\bin\
+    set start_cmd=mysqld.exe
+	
+) else (
+	echo "You must specify program to control"
+	exit
+)
 
-echo "You must specify program to control"
-goto end;
-
-REM # _______________________________________BEFORE
-:before_command
-echo start [%1] %2 %3 %4 %5 %6 %7 %8 %9
-TITLE %1
-goto %1
-
-REM # __________________________________________PHP
-:php
-cd %SRV_PATH%\php-5.6
-start /B php-cgi.exe -b 24055 -c %SRV_PATH%\php-5.6\php.ini 
-exit
-
-REM # ________________________________________NGINX
-:nginx
-cd %SRV_PATH%\nginx-1.8.0
-start /B nginx.exe %2 %3 %4 %5 %6 %7 %8 %9
-exit
-
-REM # ________________________________________MYSQL
-:mysql
-cd %SRV_PATH%\mysql-5.6\bin\
-start /B mysqld.exe %2 %3 %4 %5 %6 %7 %8 %9
+cd %SRV_PATH%\%app_path%
+start /B %start_cmd% %2 %3 %4 %5 %6 %7 %8 %9
 exit
